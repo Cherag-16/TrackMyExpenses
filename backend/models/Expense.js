debe7d8ee83b47f98cecc8423ec967a1ee0f2a15
Cherag-16 <cheragsaxena16@gmail.com>
2024-12-26 12:17:53 +0530

    Expense.js

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 
 const expenseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  amount: { type: Number, required: true },
-  category: { type: String, required: true },
-  date: { type: Date, default: Date.now },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      'Food',
+      'Transportation',
+      'Housing',
+      'Entertainment',
+      'Shopping',
+      'Healthcare',
+      'Education',
+    ],
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  }
 });
 
 const Expense = mongoose.model('Expense', expenseSchema);
-
 module.exports = Expense;
